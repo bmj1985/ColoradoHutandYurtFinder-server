bce80f310e9ab6bdf1196a177e512c6756618411
Brandon Johnson <bmj1985@gmail.com>
2018-03-06 11:11:47 -0700

    file structure

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// require('dotenv').load();
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgresql:///hutsandyurts'
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  }
+};
