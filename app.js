c69b4e5e0759e7bae5196b5fb0363804c7119c6d
Brandon Johnson <bmj1985@gmail.com>
2018-03-11 12:47:05 -0600

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 const hutsandyurts = require('./routes/hutsandyurts');
 
 app.use(bodyParser.json());
+app.use(cors);
 
 app.use('/hutsandyurts', hutsandyurts);
 
